99e196b61c4d61e5bf673d24aae88ccb6f205f1a
lckjcnWq <18819052272@163.com>
2024-10-12 10:31:11 +0800

    feat  1.添加模糊查询功能

--- routes/admin/articles.js
+++ routes/admin/articles.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const {Article} = require('../../models');
+const { Op } = require('sequelize');
+function validateIdParam(req, res, next) {
+    const { id } = req.params;
+    if (!/^\d+$/.test(id)) {
+        return next('route'); // 跳过当前路由，继续匹配其他路由
+    }
+    next();
+}
 
-/* GET home page. */
+//查询列表
 router.get('/', async function(req, res, next) {
 
     try {
@@ -28,6 +36,72 @@ router.get('/', async function(req, res, next) {
     }
 });
 
+
+//查询文章详情
+router.get('/:id', validateIdParam,async function(req, res, next) {
+    try {
+        const {id} = req.params
+        const atricle = await Article.findByPk(id)
+        if(atricle){
+            res.json({
+                status: true,
+                message: '查询文章成功',
+                data: atricle
+            });
+        }else {
+            res.status(404).json({
+                status: false,
+                message: '文章不存在'
+            });
+        }
+    } catch (e) {
+        res.json({
+            status: false,
+            message: '查询文章失败',
+            errors:[e.message]
+        });
+    }
+});
+//模糊查询文章详情
+router.get('/search/', async function(req, res, next) {
+    try {
+        console.log('Request URL:', req.url);
+        console.log('Request Query:', req.query);
+
+        const query = req.query
+        const condition = {
+            where: {},
+            order: [['id', 'DESC']]
+        };
+
+        // 添加模糊查询条件
+        if (query.title) {
+            condition.where.title = {
+                [Op.like]: `%${query.title}%`
+            };
+        }
+
+        const articles = await Article.findAll(condition)
+
+        res.json({
+            status: true,
+            message: '模糊查询文章列表成功',
+            data: {
+                articles
+            }
+        })
+
+    } catch (e) {
+        res.json({
+            status: false,
+            message: '模糊查询文章列表失败',
+            errors:[e.message]
+        });
+    }
+});
+
+
+//删除指定id的文章
 router.delete('/:id', async function(req, res, next) {
     try {
         const {id} = req.params
@@ -53,30 +127,31 @@ router.delete('/:id', async function(req, res, next) {
     }
 });
 
-router.get('/:id', async function(req, res, next) {
+//更新文章
+router.put('/:id', async function(req, res, next) {
     try {
         const {id} = req.params
         const atricle = await Article.findByPk(id)
         if(atricle){
+            await atricle.update(req.body)
             res.json({
                 status: true,
-                message: '查询文章成功',
+                message: '更新文章成功',
                 data: atricle
             });
         }else {
             res.status(404).json({
                 status: false,
-                message: '文章不存在'
+                message: '文章未找到'
             });
         }
     } catch (e) {
         res.json({
             status: false,
-            message: '查询文章失败',
+            message: '更新文章失败',
             errors:[e.message]
         });
     }
 });
-
 
 module.exports = router;
