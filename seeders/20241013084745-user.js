fa4af232718b48a87be8484aa50baf2b6b3d78b1
lukjcnWq <18819052272@163.com>
2024-10-13 17:41:08 +0800

    feat  1.用户名密码加密

--- seeders/20241013084745-user.js
+++ seeders/20241013084745-user.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const bcrypt = require('bcryptjs');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
@@ -7,7 +7,7 @@ module.exports = {
       {
         email: 'admin@clwy.cn',
         username: 'admin',
-        password: '123123',
+        password: bcrypt.hashSync('123123', 10),
         nickname: '超厉害的管理员',
         sex: 2,
         role: 100,
@@ -17,7 +17,7 @@ module.exports = {
       {
         email: 'user1@clwy.cn',
         username: 'user1',
-        password: '123123',
+        password: bcrypt.hashSync('123123', 10),
         nickname: '普通用户1',
         sex: 0,
         role: 0,
@@ -27,7 +27,7 @@ module.exports = {
       {
         email: 'user2@clwy.cn',
         username: 'user2',
-        password: '123123',
+        password: bcrypt.hashSync('123123', 10),
         nickname: '普通用户2',
         sex: 0,
         role: 0,
@@ -37,7 +37,7 @@ module.exports = {
       {
         email: 'user3@clwy.cn',
         username: 'user3',
-        password: '123123',
+        password: bcrypt.hashSync('123123', 10),
         nickname: '普通用户3',
         sex: 1,
         role: 0,
